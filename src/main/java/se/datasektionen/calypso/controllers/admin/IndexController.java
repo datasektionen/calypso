3e2875b9415202f1055271bc6de0aef0fc9c33e0
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-09-26 22:18:59 +0200

    More UI

--- src/main/java/se/datasektionen/calypso/controllers/admin/IndexController.java
+++ src/main/java/se/datasektionen/calypso/controllers/admin/IndexController.java
@@ -1,14 +1,14 @@
-package se.datasektionen.calypso.controllers.api;
+package se.datasektionen.calypso.controllers.admin;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Calypso";
+		return "redirect:/admin/list";
 	}
 
 }
