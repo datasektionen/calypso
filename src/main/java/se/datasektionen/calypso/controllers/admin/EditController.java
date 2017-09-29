b42165cc1994e4e10e687e655f31c46b87c2ddbb
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-09-29 17:33:47 +0200

    Various fixes

--- src/main/java/se/datasektionen/calypso/controllers/admin/EditController.java
+++ src/main/java/se/datasektionen/calypso/controllers/admin/EditController.java
@@ -2,12 +2,14 @@ package se.datasektionen.calypso.controllers.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import se.datasektionen.calypso.auth.DAuthUserDetails;
 import se.datasektionen.calypso.exceptions.ResourceNotFoundException;
 import se.datasektionen.calypso.models.entities.Item;
 import se.datasektionen.calypso.models.repositories.ItemRepository;
@@ -30,8 +32,14 @@ public class EditController {
 	}
 
 	@GetMapping("/admin/new")
-	public String newForm(Model model) {
-		model.addAttribute("item", new Item());
+	public String newForm(Authentication auth, Model model) {
+		DAuthUserDetails user = (DAuthUserDetails) auth.getPrincipal();
+
+		Item item = new Item();
+		item.setAuthor(user.getUser());
+		item.setAuthorDisplay(user.getName());
+
+		model.addAttribute("item", item);
 		model.addAttribute("now", LocalDateTime.now().format(formatter));
 		model.addAttribute("formatter", formatter);
 
@@ -48,8 +56,6 @@ public class EditController {
 		model.addAttribute("item", item);
 		model.addAttribute("now", LocalDateTime.now().format(formatter));
 		model.addAttribute("formatter", formatter);
-
-		// boolean ok = LocalDateTime.now().compareTo(item.getPublishDate()) >= 0;
 
 		return "edit";
 	}
