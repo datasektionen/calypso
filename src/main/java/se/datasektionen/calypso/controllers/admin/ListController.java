1d4b31b1c1a9647614cc567ff541488a92b4a341
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-10-01 16:26:02 +0200

    Switch to postgres, permission check on save, currently broken list view ACL

--- src/main/java/se/datasektionen/calypso/controllers/admin/ListController.java
+++ src/main/java/se/datasektionen/calypso/controllers/admin/ListController.java
@@ -27,10 +27,10 @@ public class ListController {
 
 	@RequestMapping("/admin/list")
 	public String index(@RequestParam(name = "itemType", defaultValue = "post") String itemType,
-	                        @RequestParam(name = "sortBy", defaultValue = "id") String sortBy,
-	                        @RequestParam(name = "sort", defaultValue = "DESC") String sort,
-	                        @RequestParam(name = "page", defaultValue = "0") int page,
-	                        Model model) {
+	                    @RequestParam(name = "sortBy", defaultValue = "id") String sortBy,
+	                    @RequestParam(name = "sort", defaultValue = "DESC") String sort,
+	                    @RequestParam(name = "page", defaultValue = "0") int page,
+	                    Model model) {
 		model.addAttribute("formatter", formatter);
 		model.addAttribute("page", page);
 		model.addAttribute("items",
--- src/main/java/se/datasektionen/calypso/models/repositories/ItemRepository.java
+++ src/main/java/se/datasektionen/calypso/models/repositories/ItemRepository.java
@@ -3,17 +3,23 @@ package se.datasektionen.calypso.models.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
 import se.datasektionen.calypso.models.entities.Item;
 import se.datasektionen.calypso.models.enums.ItemType;
 
 public interface ItemRepository extends CrudRepository<Item, Long> {
 
+	@PostFilter("hasPermission(filterObject, null)")
 	Page<Item> findAllByItemType(ItemType itemType, Pageable pageable);
-
-	Page<Item> findAllByItemTypeAndAuthor(ItemType itemType, String author, Pageable pageable);
 
 	@Override
 	@PostAuthorize("hasPermission(returnObject, null)")
 	Item findOne(Long id);
+
+	@Override
+	@PreAuthorize("hasPermission(#item, null)")
+	<S extends Item> S save(@Param("item") S item);
 }
