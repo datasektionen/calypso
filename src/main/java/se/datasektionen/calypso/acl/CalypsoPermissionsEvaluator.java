9a0a527dfb216fc11b0c9bd40676f737e6d9a791
Rafael Oliveira <rmfseo@kth.se>
2024-04-19 18:45:00 +0200

    refactor: generalize perms evaluator to operate on SecurityTarget
    Relying on an abstract interface rather than a specific class paves the
    way for supporting multiple ACL-controller entities without having to
    change this method every time.

--- src/main/java/se/datasektionen/calypso/acl/CalypsoPermissionsEvaluator.java
+++ src/main/java/se/datasektionen/calypso/acl/CalypsoPermissionsEvaluator.java
@@ -4,7 +4,6 @@ import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import se.datasektionen.calypso.auth.DAuthUserDetails;
-import se.datasektionen.calypso.models.entities.Item;
 
 import java.io.Serializable;
 import java.util.Optional;
@@ -12,30 +11,31 @@ import java.util.Optional;
 public class CalypsoPermissionsEvaluator implements PermissionEvaluator {
 
 	@Override
-	public boolean hasPermission(Authentication auth, Object target, Object permission) {
+	public boolean hasPermission(Authentication auth, Object maybeTarget, Object permission) {
 		var user = (DAuthUserDetails) auth.getPrincipal();
 
-		if (target instanceof Optional) {
-			target = ((Optional<Item>) target).get();
+		// it may or may not be in an Optional
+		if (maybeTarget instanceof Optional opt && opt.isPresent()) {
+			maybeTarget = opt.get();
 		}
 
-		if (target instanceof Item) {
+		if (maybeTarget instanceof SecurityTarget target) {
 			var editor = user.getAuthorities().contains(new SimpleGrantedAuthority("editor"));
-			var item = (Item) target;
 
 			// Editors always have access
 			if (editor)
 				return true;
 
-			// By now, we know the user isn't editor, thus they can only access their own content
-			return user.getUser().equals(item.getAuthor());
+			return user.getUser().equals(target.getAuthor());
 		}
 
 		return false;
 	}
 
 	@Override
-	public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
-		throw new UnsupportedOperationException("TargetID based permission checking not implemented in this application.");
+	public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType,
+			Object permission) {
+		throw new UnsupportedOperationException(
+				"TargetID based permission checking not implemented in this application.");
 	}
 }
