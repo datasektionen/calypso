898656060e8b514d61252e0d69c3575a372eaa6b
Rafael Oliveira <rmfseo@kth.se>
2024-09-12 19:35:47 +0200

    feat(rss): add parameter to include only important items
    Fixes #28, at least somewhat

--- src/main/java/se/datasektionen/calypso/controllers/api/FeedController.java
+++ src/main/java/se/datasektionen/calypso/controllers/api/FeedController.java
@@ -1,9 +1,11 @@
 package se.datasektionen.calypso.controllers.api;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import se.datasektionen.calypso.feeds.IcalFeed;
 import se.datasektionen.calypso.feeds.rss.RssFeeds;
@@ -30,13 +32,13 @@ public class FeedController {
 	}
 
 	@RequestMapping(produces = "application/rss+xml", method = RequestMethod.GET, value = "/rss")
-	public RssView rssFeedSwedish() {
-		return rssFeeds.swedishFeed();
+	public RssView rssFeedSwedish(@RequestParam Optional<Boolean> important) {
+		return rssFeeds.swedishFeed(important.orElse(false));
 	}
 
 	@RequestMapping(produces = "application/rss+xml", method = RequestMethod.GET, value = "/rss_en")
-	public RssView rssFeedEnglish() {
-		return rssFeeds.englishFeed();
+	public RssView rssFeedEnglish(@RequestParam Optional<Boolean> important) {
+		return rssFeeds.englishFeed(important.orElse(false));
 	}
 
 }
