datasektionen/aaallt2

Update traefik config

job.nomad.hcl
@@ -11,11 +11,10 @@ job "aaallt2" {
       port     = "http"
       provider = "nomad"
       tags = [
-        "traefik-external.enable=true",
-        "traefik-external.http.routers.aaallt2.rule=Host(`aaallt.datasektionen.se`)||HostRegexp(`{[^.]+}.datasektionen.se`)",
-        "traefik-external.http.routers.aaallt2.priority=1",
-        "traefik-external.http.routers.aaallt2.entrypoints=websecure",
-        "traefik-external.http.routers.aaallt2.tls.certresolver=default",
+        "traefik.enable=true",
+        "traefik.http.routers.aaallt2.rule=Host(`aaallt.datasektionen.se`)||HostRegexp(`{[^.]+}.datasektionen.se`)",
+        "traefik.http.routers.aaallt2.priority=1",
+        "traefik.http.routers.aaallt2.tls.certresolver=default",
       ]
     }
 

datasektionen/calypso

job.nomad.hcl
@@ -11,10 +11,9 @@ job "calypso" {
       port     = "http"
       provider = "nomad"
       tags = [
-        "traefik-external.enable=true",
-        "traefik-external.http.routers.calypso.rule=Host(`calypso.datasektionen.se`)",
-        "traefik-external.http.routers.calypso.entrypoints=websecure",
-        "traefik-external.http.routers.calypso.tls.certresolver=default",
+        "traefik.enable=true",
+        "traefik.http.routers.calypso.rule=Host(`calypso.datasektionen.se`)",
+        "traefik.http.routers.calypso.tls.certresolver=default",
 
         "traefik-internal.enable=true",
         "traefik-internal.http.routers.calypso.rule=Host(`calypso.nomad.dsekt.internal`)",