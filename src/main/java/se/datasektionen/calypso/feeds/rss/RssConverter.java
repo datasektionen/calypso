f1344968a6d3a392529f81751b5ee845ea01ee35
chaj <chaithetea@proton.me>
2025-04-17 19:42:51 +0200

    add: a cool cat :3

--- src/main/java/se/datasektionen/calypso/feeds/rss/RssConverter.java
+++ src/main/java/se/datasektionen/calypso/feeds/rss/RssConverter.java
@@ -2,9 +2,12 @@ package se.datasektionen.calypso.feeds.rss;
 
 import com.rometools.rome.feed.rss.Content;
 import com.rometools.rome.feed.rss.Description;
+import com.rometools.rome.feed.rss.Enclosure;
+
 import se.datasektionen.calypso.models.entities.Item;
 
 import java.util.function.Function;
+import java.util.List;
 
 import static se.datasektionen.calypso.feeds.DateUtils.ldtToDate;
 
@@ -20,6 +23,10 @@ public class RssConverter {
 															 Function<Item, String> linkMapper) {
 		var rssItem = new com.rometools.rome.feed.rss.Item();
 
+		var imageEnclosure = new Enclosure();
+		imageEnclosure.setType("image/png");
+		imageEnclosure.setUrl("https://wallpapercave.com/wp/wp4033948.jpg"); //meow :3
+
 		var description = new Description();
 		description.setType(Content.HTML);
 		description.setValue(contentMapper.apply(item));
@@ -29,6 +36,7 @@ public class RssConverter {
 		rssItem.setDescription(description);
 		rssItem.setPubDate(ldtToDate(item.getPublishDate()));
 		rssItem.setLink(linkMapper.apply(item));
+		rssItem.setEnclosures(List.of(imageEnclosure));
 		return rssItem;
 	}
 
