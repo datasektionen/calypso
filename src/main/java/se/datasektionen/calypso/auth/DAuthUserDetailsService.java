db3497b1ee421852caf4dd245a54febc65bc2d06
chaj <chaithetea@proton.me>
2025-09-25 21:50:36 +0200

    migrate: actually grab data properly

--- src/main/java/se/datasektionen/calypso/auth/DAuthUserDetailsService.java
+++ src/main/java/se/datasektionen/calypso/auth/DAuthUserDetailsService.java
@@ -1,10 +1,10 @@
 package se.datasektionen.calypso.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,12 +12,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.web.client.RestTemplate;
 import se.datasektionen.calypso.auth.entities.DAuthResponse;
-import se.datasektionen.calypso.auth.entities.dfunkt.DFunktResponse;
-import se.datasektionen.calypso.auth.entities.dfunkt.Mandate;
-import se.datasektionen.calypso.auth.entities.dfunkt.Role;
+import se.datasektionen.calypso.auth.entities.memberships.Group;
+import se.datasektionen.calypso.auth.entities.permissions.Permission;
 import se.datasektionen.calypso.config.Config;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -65,10 +63,12 @@ public class DAuthUserDetailsService implements AuthenticationUserDetailsService
 		headers.set("Authorization", "Bearer " + config.getHiveApiKey());
 
 		// Read permissions from hive
-		var permissions = new RestTemplate()
-				.exchange(permissionsUrl, HttpMethod.GET, new HttpEntity<>(null, headers), String[].class);
-		List<GrantedAuthority> authorities = Arrays.stream(permissions.getBody())
-				.map(SimpleGrantedAuthority::new)
+		var permissionsHttp = new RestTemplate()
+				.exchange(permissionsUrl, HttpMethod.GET, new HttpEntity<>(null, headers), new ParameterizedTypeReference<List<Permission>>() {}) ;
+		var authorities = permissionsHttp
+				.getBody()
+				.stream()
+				.map(p -> new SimpleGrantedAuthority(p.getId()))
 				.collect(Collectors.toList());
 
 		// Try to get mandates from hive
@@ -76,14 +76,12 @@ public class DAuthUserDetailsService implements AuthenticationUserDetailsService
 		var mandatesUrl = "https://hive.datasektionen.se/api/v1/tagged/author-pseudonym/memberships/"+user;
 		try {
 			var mandatesHttp = new RestTemplate()
-					.exchange(mandatesUrl, HttpMethod.GET, new HttpEntity<>(null, headers), DFunktResponse.class);
-
+					.exchange(mandatesUrl, HttpMethod.GET, new HttpEntity<>(null, headers), new ParameterizedTypeReference<List<Group>>() {});
 			mandates = mandatesHttp
 					.getBody()
-					.getMandates()
 					.stream()
-					.map(Mandate::getRole)
-					.collect(Collectors.toMap(Role::getIdentifier, Role::getTitle));
+					.collect(Collectors.toMap(Group::getGroup_id, Group::getGroup_name));
+					// .collect(Collectors.toMap(Group::getGroup_id, Group::getGroup_name));
 
 			System.out.println(mandates.toString());
 		} catch (Exception ignore) {
