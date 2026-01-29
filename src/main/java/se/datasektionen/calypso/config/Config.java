0cc90e9eb605dc0363d11550b67ebc3eec9c4121
chaj <chaithetea@proton.me>
2026-01-29 19:12:56 +0100

    wow: remove redundant things

--- src/main/java/se/datasektionen/calypso/config/Config.java
+++ src/main/java/se/datasektionen/calypso/config/Config.java
@@ -9,39 +9,28 @@ import lombok.Getter;
 @Getter
 public class Config {
 	private final String baseUrl;
-	//TODO
-	// private final String loginApiKey;
-	// private final String loginFrontendUrl;
-	// private final String loginApiUrl;
 
 	private final String oidcProvider;
 	private final String oidcId;
 	private final String oidcSecret;
-	private final String redirectUrl; //TODO unsure if this should even be here
+	private final String redirectUrl;
 	private final String darkmodeUrl;
 	private final String hiveApiKey;
 	private final String hiveApiUrl;
 
 	public Config(
 		@Value("${APPLICATION_URL}") String baseUrl,
-		// @Value("${LOGIN_KEY}") String loginApiKey,
-		// //also TODO
-		// @Value("${LOGIN_FRONTEND_URL:https://login.datasektionen.se}") String loginFrontendUrl,
-		// @Value("${LOGIN_API_URL:https://login.datasektionen.se}") String loginApiUrl,
 
 		@Value("${OIDC_PROVIDER:https://sso.datasektionen.se}") String OIDCProvider,
 		@Value("${OIDC_ID}") String OIDCId,
 		@Value("${OIDC_SECRET}") String OIDCSecret,
-		@Value("${REDIRECT_URL}") String redirectUrl, //i mean this is just baseUrl?
+		@Value("${REDIRECT_URL}") String redirectUrl,
 
 		@Value("${DARKMODE_URL:https://darkmode.datasektionen.se}") String darkmodeUrl,
 		@Value("${HIVE_API_KEY}") String hiveApiKey,
 		@Value("${HIVE_URL:https://hive.datasektionen.se}") String hiveApiUrl
 	) {
 		this.baseUrl = baseUrl;
-		// this.loginApiKey = loginApiKey;
-		// this.loginFrontendUrl = loginFrontendUrl;
-		// this.loginApiUrl = loginApiUrl;
 		this.oidcProvider = OIDCProvider;
 		this.oidcId = OIDCId;
 		this.oidcSecret = OIDCSecret;
